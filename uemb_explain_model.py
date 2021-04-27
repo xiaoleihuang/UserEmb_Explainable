2dd88e094c937a4931cdf5fc7e406ab0da832b94
Xiaolei Huang <xiaoleihuangccnu@gmail.com>
2021-04-27 15:19:23 -0500

    updates

--- uemb_explain_model.py
+++ uemb_explain_model.py
@@ -61,6 +61,20 @@ def build_gru_model(params=None):
             trainable=params['word_emb_train'], name='word_emb'
         )
 
+    # load weights if concept embedding path is given
+    if os.path.exists(params['concept_emb_path']):
+        weights = np.load(params['concept_emb_path'])
+        concept_emb = keras.layers.Embedding(
+            params['vocab_size'], weights.shape[1],
+            weights=[weights],
+            trainable=params['word_emb_train'], name='concept_emb'
+        )
+    else:
+        concept_emb = keras.layers.Embedding(
+            params['vocab_size'], params['emb_dim'],
+            trainable=params['word_emb_train'], name='concept_emb'
+        )
+
     # load weights if user embedding path is given
     if 'pretrained_uemb' in params and os.path.exists(params['pretrained_uemb']):
         user_emb = keras.layers.Embedding(
@@ -73,6 +87,58 @@ def build_gru_model(params=None):
             params['user_size'], params['emb_dim'],
             trainable=params['user_emb_train'], name='user_emb'
         )
+
+    '''
+    User & Documents
+    '''
+    user1_rep = user_emb(user1_input)
+    doc_rep = word_emb(doc_input)
+
+    # GRU reads through words to generate document representation
+    gru_encoder = keras.layers.GRU(params['emb_dim'], )(doc_rep)
+    gru_encoder = keras.layers.Dropout(params['dp_rate'])(gru_encoder)
+
+    # dot product between document and user vectors
+    user_doc_dot = keras.layers.dot(
+        [user1_rep, gru_encoder], axes=-1
+    )
+    user_doc_dot = keras.layers.Reshape((1,))(user_doc_dot)
+    user_doc_pred = keras.layers.Dense(
+        1, activation='sigmoid', name='user_doc_pred'
+    )(user_doc_dot)
+
+    '''
+    User & Concepts
+    '''
+    user2_rep = user_emb(user2_input)
+    concept_rep = concept_emb(concept_input)
+    user_concept_dot = keras.layers.dot(
+        [user2_rep, concept_rep], axes=-1
+    )
+    user_concept_dot = keras.layers.Reshape((1,))(user_concept_dot)
+    user_concept_pred = keras.layers.Dense(
+        1, activation='sigmoid', name='user_concept_pred'
+    )(user_concept_dot)
+
+    '''Compose Multitask model'''
+    ud_model = keras.models.Model(
+        inputs=[user1_input, doc_input],
+        outputs=user_doc_pred
+    )
+    ud_model.compile(
+        loss='binary_crossentropy',
+        optimizer=keras.optimizers.RMSprop(lr=params['lr'])
+    )
+    uc_model = keras.models.Model(
+        inputs=[user2_input, concept_input],
+        outputs=user_concept_pred
+    )
+    uc_model.compile(
+        loss='binary_crossentropy',
+        optimizer=keras.optimizers.Adam(lr=params['lr'])
+    )
+
+    return ud_model, uc_model
 
 
 class DannGru(nn.Module):
@@ -92,10 +158,23 @@ class DannGru(nn.Module):
 
         # word embeddings
         if 'pretrained_wemb' in self.params and os.path.exists(self.params['pretrained_wemb']):
-            self.wemb = nn.Embedding.from_pretrained(self.params['pretrained_uemb'])
+            self.wemb = nn.Embedding.from_pretrained(
+                self.params['pretrained_uemb'])
+            self.wemb.requires_grad_(requires_grad=False)  # freeze the weight update
         else:
             self.wemb = nn.Embedding(
                 self.params['vocab_size'], self.params['emb_dim']
+            )
+            torch.nn.init.kaiming_uniform_(self.uemb.weight, a=np.sqrt(5))
+
+        # concept embeddings
+        if os.path.exists(self.params['concept_emb_path']):
+            self.cemb = nn.Embedding.from_pretrained(
+                self.params['pretrained_uemb'])
+            self.cemb.requires_grad_(requires_grad=False)  # freeze the weight update
+        else:
+            self.cemb = nn.Embedding(
+                self.params['concept_size'], self.params['emb_dim']
             )
             torch.nn.init.kaiming_uniform_(self.uemb.weight, a=np.sqrt(5))
 
@@ -110,10 +189,24 @@ class DannGru(nn.Module):
 
         # define self attention networks
         self.att_nn = None
-        pass
 
-    def forward(self):
-        pass
+    def forward(self, **kwargs):
+        input_doc_ids = kwargs['input_doc_ids']
+        input1_uids = kwargs['input1_uids']
+        users1 = self.uemb(input1_uids)
+        doc_embs = self.wemb(input_doc_ids)
+        _, gru_embs = self.gru(doc_embs)
+        gru_embs = torch.cat((gru_embs[0, :, :], gru_embs[1, :, :]), -1)
+        # dot product between user and docs
+        user_doc_sim = torch.sum(users1 * gru_embs, -1)
+
+        input2_uids = kwargs['input2_uids']
+        input_concept_ids = kwargs['input_concept_ids']
+        users2 = self.uemb(input2_uids)
+        concept_embs = self.cemb(input_concept_ids)
+        # dot product between user and concepts
+        user_concept_sim = torch.sum(users2 * concept_embs, -1)
+        return user_doc_sim, user_concept_sim
 
 
 class DannBert(nn.Module):
--- uemb_explain_train.py
+++ uemb_explain_train.py
@@ -2,9 +2,14 @@ import argparse
 import os
 import json
 import pickle
+import sys
 
 from nltk.tokenize import RegexpTokenizer
 from transformers import BertTokenizer
+import numpy as np
+from tqdm import tqdm
+import gensim
+from keras.preprocessing.sequence import pad_sequences
 # load data
 # load concepts
 # import model
@@ -42,7 +47,7 @@ def concept_preprocessor(concepts):
     pass
 
 
-def build_concept_encoder(concepts):
+def build_concept_tokenizer(concepts):
     # this will help build concept embeddings
 
     pass
@@ -56,23 +61,71 @@ def data_loader(**kwargs):
     word_tkn_path = kwargs['word_tkn_path']
     bert_tkn = BertTokenizer.from_pretrained(kwargs['bert_name'])
 
+    # load dataset
+    with open(data_path) as dfile:
+        for line in dfile:
+            user_entity = json.loads(line)
+
     # load concepts
+
     pass
 
 
-def main():
+def main(params):
     pass
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process model parameters.')
+    parser.add_argument('--method', type=str, help='GRU or BERT')
     parser.add_argument('--dname', type=str, help='The data\'s name')
     parser.add_argument('--lr', type=float, help='Learning rate', default=.0001)
     parser.add_argument('--ng_num', type=int, help='Number of negative samples', default=1)
     parser.add_argument('--batch_size', type=int, help='Batch size', default=16)
     parser.add_argument('--max_len', type=int, help='Max length', default=512)
     parser.add_argument('--emb_dim', type=int, help='Embedding dimensions', default=300)
+    parser.add_argument('--device', type=str, help='cpu or cuda')
     args = parser.parse_args()
 
+    if args.method not in ['gru2user', 'bert2user']:
+        print('Method {} is not supported.'.format(args.method))
+        sys.exit()
 
+    data_dir = './data/processed_data/{}/'.format(args.dname)
+    odir = './resources/embedding/{}/'.format(args.dname)
+    if not os.path.exists(odir):
+        os.mkdir(odir)
+    odir = odir + '{}/'.format(args.method)
+    if not os.path.exists(odir):
+        os.mkdir(odir)
+
+    if args.device == 'cpu':
+        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # for cpu usage
+        os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+    parameters = {
+        'batch_size': 32,
+        'user_size': -1,
+        'word_emb_path': './resources/embedding/{}/word_emb.npy'.format(args.dname),
+        'user_emb_path': './resources/embedding/{}/user_emb.npy'.format(args.dname),
+        'user_task_weight': 1,
+        'concept_task_weight': 1,
+        'epochs': 15,
+        'optimizer': 'adam',
+        'lr': args.lr,
+        'negative_sample': args.ng_num,
+        'max_len': args.max_len,
+        # 'emilyalsentzer/Bio_ClinicalBERT'
+        # 'bionlp/bluebert_pubmed_mimic_uncased_L-12_H-768_A-12'
+        # 'bionlp/bluebert_pubmed_uncased_L-12_H-768_A-12'
+        'bert_name': 'bionlp/bluebert_pubmed_mimic_uncased_L-12_H-768_A-12',
+        'decay_rate': .9,
+        'warm_steps': 33,
+        'emb_dim': args.emb_dim,
+        'dp_rate': .2,
+        'dname': args.dname,
+        'encode_dir': data_dir,
+        'odir': odir,
+        'device': args.device
+    }
     pass

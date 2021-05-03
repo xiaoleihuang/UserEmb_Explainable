c88727d80c008cc8fe6c1ca4bb7d1f15acae0520
Xiaolei Huang <xiaoleihuangccnu@gmail.com>
2021-05-02 21:49:26 -0500

    updates on evaluation

--- eval_helper.py
+++ eval_helper.py
@@ -4,6 +4,12 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # for cpu usage
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 for dname in ['diabetes', 'mimic-iii']:  # 'diabetes', 'mimic-iii'
-    for model in ['bert2user', 'bert_gru2user']:  # 'lda2user', 'doc2user', 'user2vec', 'gru2user',
+    # 'suisil2user', 'deeppatient2user', 'lda2user', 'doc2user', 'user2vec',
+    # 'word2user', 'caue_gru', 'caue_bert',
+    methods = [
+        'suisil2user', 'deeppatient2user', 'lda2user', 'doc2user',
+        'word2user',  # 'user2vec', 'caue_gru', 'caue_bert',
+    ]
+    for model in methods:
         job_str = 'python evaluator.py --dname {} --model {}'.format(dname, model)
         process = subprocess.Popen(job_str, shell=True)

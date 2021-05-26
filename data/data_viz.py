efeff99396e1a6f3ed52e99810c7268b79af10de
Xiaolei Huang <xiaoleihuangccnu@gmail.com>
2021-05-26 15:53:01 -0400

    update visualization

--- data/data_viz.py
+++ data/data_viz.py
@@ -198,6 +198,6 @@ if __name__ == '__main__':
 
     for dname in ['diabetes', 'mimic-iii']:
         for method in ['caue_gru', 'user2vec', 'suisil2user']:
-            # user_viz_phenotype(dname, method)
+            user_viz_phenotype(dname, method)
             user_mortality_viz(dname, method)
 

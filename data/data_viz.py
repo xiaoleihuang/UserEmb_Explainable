7f47debe9838c34ffe09a4a43344793451db74ce
Xiaolei Huang <xiaoleihuangccnu@gmail.com>
2021-05-26 14:52:02 -0400

    update visualization

--- data/data_viz.py
+++ data/data_viz.py
@@ -31,7 +31,7 @@ def analysis_viz(dpath, data_name='Diabetes'):
     subprocess.run("pdfcrop {} {}".format(opath, opath), shell=True, text=True)
 
 
-def user_viz(data_name, method_name):
+def user_viz_phenotype(data_name, method_name):
     emb_dir = '../resources/embedding/{}/{}/'.format(data_name, method_name)
     data_dir = '../data/processed_data/{}/'.format(data_name)
     stats_path = '../resources/analyze/{}_stats.json'.format(data_name)
@@ -120,6 +120,75 @@ def user_viz(data_name, method_name):
     plt.close()
 
 
+def user_mortality_viz(data_name, method_name):
+    if data_name != 'mimic-iii':
+        return
+
+    emb_dir = '../resources/embedding/{}/{}/'.format(data_name, method_name)
+    data_dir = '../data/processed_data/{}/'.format(data_name)
+
+    # load user encoder
+    with open(data_dir + 'user_encoder.json') as dfile:
+        user_encoder = json.load(dfile)
+
+    # load tag stats
+    tag_encoder = json.load(open(data_dir + 'mortality.json'))
+    user_tags = dict()
+    with open(data_dir+data_name + '.json') as dfile:
+        for line in dfile:
+            user_info = json.loads(line)
+            raw_uid = user_info['uid']
+            uid = user_encoder[user_info['uid']]
+            if raw_uid not in tag_encoder:
+                user_tags[uid] = -1
+            else:
+                user_tags[uid] = tag_encoder[raw_uid]
+
+    # write down the results
+    inpath = '../resources/analyze/{}/{}_mortality_viz.tsv'.format(data_name, method_name)
+    if not os.path.exists(inpath):
+        # load user embeddings
+        # load the user embeddings, default to load the user.npy
+        if os.path.exists(emb_dir + 'user.npy'):
+            uembs = np.load(emb_dir + 'user.npy')
+        else:
+            # this assumes using .txt file
+            uembs = [[]] * len(user_tags)
+            with open(emb_dir + 'user.txt') as dfile:
+                for line in dfile:
+                    line = line.strip().split('\t')
+                    if len(line) != 2:
+                        continue
+                    uid = user_encoder[line[0]]
+                    uembs[uid] = [float(item) for item in line[1].split()]
+            uembs = np.asarray(uembs)
+
+        tsne = TSNE(n_components=2, n_jobs=-1)
+        uembs = tsne.fit_transform(uembs)
+
+        with open(inpath, 'w') as wfile:
+            wfile.write('x\ty\tlabel\n')
+            for udx in user_tags:
+                if user_tags[udx] == -1:
+                    continue
+                wfile.write('{}\t{}\t{}\n'.format(uembs[udx][0], uembs[udx][1], user_tags[udx]))
+
+    # visualization
+    df = pd.read_csv(inpath, sep='\t')
+    a4_dims = (12.27, 12.27)
+    # cmap = sns.cubehelix_palette(as_cmap=True)
+    fig, ax = plt.subplots(figsize=a4_dims)
+
+    opath = '../resources/analyze/{}/{}_mortality_viz.pdf'.format(data_name, method_name)
+    viz_plot = sns.scatterplot(data=df, x='x', y='y', hue='label', ax=ax)
+    viz_plot.set_ylabel('X', fontsize=20)
+    viz_plot.set_xlabel('Y', fontsize=20)
+    # plt.setp(ax.get_legend().get_texts(), fontsize=22)
+    # plt.setp(ax.get_legend().get_title(), fontsize=22)
+    viz_plot.figure.savefig(opath, format='pdf')
+    plt.close()
+
+
 if __name__ == '__main__':
     # quant_path = '../resources/analyze/quant.csv'
     # for dname in ['Diabetes', 'MIMIC-III']:
@@ -127,4 +196,6 @@ if __name__ == '__main__':
 
     for dname in ['diabetes', 'mimic-iii']:
         for method in ['caue_gru', 'user2vec', 'suisil2user']:
-            user_viz(dname, method)
+            # user_viz_phenotype(dname, method)
+            user_mortality_viz(dname, method)
+
